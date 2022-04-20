972cbe7d5a51d71654c6e2cf6b250a2523111c9d
zznomuxu <jiaxin.shang@outlook.com>
2022-04-20 17:51:35 +0800

    Site updated: 2022-04-20 17:51:35

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -1,7 +1,7 @@
 var a_idx = 0;
 jQuery(document).ready(function ($) {
     $("body").click(function (e) {
-        var a = new Array("来康康", "爱了爱了", "真不错");
+        var a = new Array("嘻嘻", "我是周周", "你真漂亮","哈哈哈哈哈哈");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
